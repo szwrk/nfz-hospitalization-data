acb775ad57de4559d88686b6554b31ea8dc88ae2
Arek <arek@wilamowski.net>
2024-05-16 17:07:40 +0200

    add install bash script

---
+++ install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+db_tns_cdb="192.168.0.51:1521/free"
+db_tns_datamart_pdb="192.168.0.51:1521/datamart"
+
+dba_name="sys"
+dev_name="c##jsmith"
+
+read -p 'Demo mode: Load a small number of records? (y/n): ' demo_mode
+read -s -p 'Enter dba password : ' dba_pass
+echo ' '
+read -s -p 'Enter dev password : ' dev_pass
+
+
+check_success() {
+    if [ $? -ne 0 ]; then
+        echo "ERROR: The previous command failed. Exiting."
+        exit 1
+    fi
+}
+echo ""
+echo "======================================"
+echo " Starting Drop Database Script"
+echo "======================================"
+cd sql || exit 1
+
+sql -S ${dba_name}@${db_tns_cdb} AS SYSDBA @0-drop.sql<<EOF
+${dba_pass}
+exit
+EOF
+
+check_success
+cd ..
+echo "Drop Database Script Completed"
+echo ""
+
+echo "======================================"
+echo " Starting Database Objects Setup Script"
+echo "======================================"
+cd sql || exit 1
+sql -S ${dba_name}@${db_tns_cdb} AS SYSDBA @1-Setup-db.sql<<EOF
+${dba_pass}
+exit
+EOF
+cd ..
+echo "Database Objects Setup Script Completed"
+echo ""
+
+echo "======================================"
+echo " Starting Data Mart Build Script"
+echo "======================================"
+cd sql || exit 1
+sql -S ${dba_name}@${db_tns_cdb} AS SYSDBA @2-create-mart.sql<<EOF
+${dba_pass}
+exit
+EOF
+cd ..
+echo "Data Mart Build Script Completed"
+echo ""
+
+echo "======================================"
+echo " Loading Data"
+echo "======================================"
+# Execute SQL*Loader for 2019-2022
+
+if [ "$demo_mode" = "y" ]; then
+    echo "Use demo dataset."
+    sqlldr ${dev_name}/${dev_pass}@${db_tns_datamart_pdb} control=ctl/demo_nfz_hospitalizations_2019-2022.ctl log=log/demo_nfz_hospitalizations_2019-2022.log
+else
+    echo "Downloading full csv (>1GB)..."
+    pwd
+    # 2019-2021
+    mkdir -p data-full
+
+    curl -o data-full/nfz_hospitalizations_2019_2021.7z https://api.dane.gov.pl/media/resources/20230217/hospitalizacje.7z
+    cd data-full
+    7z x hospitalizacje.7z -odata-full    
+    mv hospitalizacje.csv nfz_hospitalizations_2019_2021.csv
+    # 2022
+    curl -o data-full/nfz_hospitalizations_2022.csv https://api.dane.gov.pl/media/resources/20240123/hospitalizacje_2022.csv    
+    mv hospitalizacje.csv nfz_hospitalizations_2022.csv
+    # Load data
+    cd ..
+    sqlldr ${dev_name}/${dev_pass}@${db_tns_datamart_pdb} control=ctl/nfz_hospitalizations_2019-2022.ctl log=log/nfz_hospitalizations_2019-2022.log
+fi
+echo "Data Load Completed"
+echo ""
