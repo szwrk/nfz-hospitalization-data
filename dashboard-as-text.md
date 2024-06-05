b7543def7fee0288abcd0c1eea30ac63d75847ec
Arek <arek@wilamowski.net>
2024-06-05 22:27:24 +0200

    feat: add report - monthly hospitalization comparision

--- dashboard-as-text.md
+++ dashboard-as-text.md
@@ -21,7 +21,7 @@ ___________________________________
 
 **2. Yearly Hospitalization Comparison**
 
-- _Compare hospitalizations from the previous year at the same point in time (on the same date)_
+- _Compare the number of hospitalizations from the previous year at the same point in time (on the same date)_
 
 ```sql
 SQL> SELECT value as BALANCE 
@@ -33,4 +33,34 @@ Output:
 BALANCE
 ______________________________
  +204084
+```
+
+**3. Monthly Hospitalization Comparison**
+
+- _Compare the number of hospitalizations monthly. Show the current count for January_
+
+```sql
+SQL> SELECT 
+    r.period
+    , r.month_count
+    , r.diff_prev_month
+FROM RPT_HOSPS_MONTHLY_DIFFS r
+WHERE r.year = 2022
+;
+```
+Output:
+```
+PERIOD                                            MONTH_COUNT DIFF_PREV_MONTH
+------------------------------------------------- ----------- ---------------
+2022/01                                                236502          236502
+2022/02                                                233503           -2999
+2022/03                                                291638           58135
+2022/04                                                302054           10416
+2022/05                                                305431            3377
+2022/06                                                306737            1306
+2022/07                                                303161           -3576
+2022/08                                                296762           -6399
+2022/09                                                304887            8125
+2022/10                                                313383            8496
+2022/11                                                302807          -10576
 ```
--- sql/5-create-reports-as-analyst.sql
+++ sql/5-create-reports-as-analyst.sql
@@ -1,62 +1,110 @@
-CONNECT c##jdoe/oracle@datamart;
+CONNECT c##jdoe/ORACLE@datamart;
 
 -- Total Hospitalizations Report
 CREATE OR REPLACE VIEW c##jdoe.rpt_totalhosp AS
-SELECT to_char(
-      COUNT(1),'FM999,999,999'
-   ) AS value 
-FROM dm_nfzhosp.f_hospitalizations
-;
-SELECT value
-FROM rpt_totalhosp
-;
-SELECT TO_NUMBER(value,'FM999,999,999' ) --for apache superset
-FROM rpt_totalhosp
-;
+   SELECT
+      to_char(COUNT(1),
+              'FM999,999,999') AS value
+   FROM
+      dm_nfzhosp.f_hospitalizations;
+
+SELECT
+   value
+FROM
+   rpt_totalhosp;
+
+SELECT
+   TO_NUMBER(value, 'FM999,999,999') --for apache superset
+FROM
+   rpt_totalhosp;
 -- Yearly_hospitalizations_comparison Report
 CREATE OR REPLACE VIEW c##jdoe.rpt_yearly_hosps_comp AS
-WITH
-previous_year(PERIOD, hosp_count) AS (
-SELECT to_char(EXTRACT(YEAR FROM TO_DATE('2022-06-04','YYYY-MM-DD')) -1) AS PERIOD, COUNT(1) AS hosp_count
-   FROM dm_nfzhosp.f_hospitalizations facts
-   LEFT JOIN dm_nfzhosp.dim_date dimdate ON facts.dim_date_id = dimdate.id_date
-   WHERE dimdate.YEAR = EXTRACT(YEAR FROM TO_DATE('2022-06-04','YYYY-MM-DD')) - 1 --assume that today NOW sysdate is '2022-06-04'
-      AND dimdate.MONTH < EXTRACT(MONTH FROM TO_DATE('2022-06-04','YYYY-MM-DD'))  
-)
-,current_year(PERIOD, hosp_count) AS (
- SELECT to_char(EXTRACT(YEAR FROM TO_DATE('2022-06-04','YYYY-MM-DD'))), COUNT(1)
-   FROM dm_nfzhosp.f_hospitalizations facts
-   LEFT JOIN dm_nfzhosp.dim_date dimdate ON facts.dim_date_id = dimdate.id_date
-   WHERE dimdate.YEAR = EXTRACT(YEAR FROM TO_DATE('2022-06-04','YYYY-MM-DD'))
-      AND dimdate.MONTH < EXTRACT(MONTH FROM TO_DATE('2022-06-04','YYYY-MM-DD'))
-)
+   WITH previous_year (
+      period,
+      hosp_count
+   ) AS (
+      SELECT
+         to_char(EXTRACT(YEAR FROM TO_DATE('2022-06-04', 'YYYY-MM-DD')) - 1) AS period,
+         COUNT(1)                                                            AS hosp_count
+      FROM
+         dm_nfzhosp.f_hospitalizations facts
+         LEFT JOIN dm_nfzhosp.dim_date           dimdate ON facts.dim_date_id = dimdate.id_date
+      WHERE
+            dimdate.year = EXTRACT(YEAR FROM TO_DATE('2022-06-04', 'YYYY-MM-DD')) - 1 --assume that today NOW sysdate is '2022-06-04'
+         AND dimdate.month < EXTRACT(MONTH FROM TO_DATE('2022-06-04', 'YYYY-MM-DD'))
+   ), current_year (
+      period,
+      hosp_count
+   ) AS (
+      SELECT
+         to_char(EXTRACT(YEAR FROM TO_DATE('2022-06-04', 'YYYY-MM-DD'))),
+         COUNT(1)
+      FROM
+         dm_nfzhosp.f_hospitalizations facts
+         LEFT JOIN dm_nfzhosp.dim_date           dimdate ON facts.dim_date_id = dimdate.id_date
+      WHERE
+            dimdate.year = EXTRACT(YEAR FROM TO_DATE('2022-06-04', 'YYYY-MM-DD'))
+         AND dimdate.month < EXTRACT(MONTH FROM TO_DATE('2022-06-04', 'YYYY-MM-DD'))
+   )
+   SELECT
+      to_char((
+         SELECT
+            hosp_count
+         FROM
+            current_year
+      ) -(
+         SELECT
+            hosp_count
+         FROM
+            previous_year
+      ), 'S9999999') AS value
+   FROM
+      dual;
+
 SELECT
-   to_char(
-      (SELECT hosp_count FROM current_year) - (SELECT hosp_count FROM previous_year)
-   ,'S9999999') AS value
-FROM dual
-;
-SELECT value AS BALANCE
-FROM rpt_yearly_hosps_comp
-;
---Waterfall
-CREATE VIEW v_hosps_monthly_diffs
-WITH monthly_counts AS (
-    SELECT
-        HOSP_PERIOD,
-        COUNT(1) AS month_count
-    FROM
-        v_hospitalizations_2022
-    GROUP BY
-        HOSP_PERIOD
-)
-SELECT
-    HOSP_PERIOD,
-    month_count,
-    nvl(month_count - LAG(month_count, 1) OVER (ORDER BY HOSP_PERIOD),month_count) AS diff_with_prev_month
+   value AS balance
 FROM
-    monthly_counts
+   rpt_yearly_hosps_comp;
+
+--Waterfall chart
+--rpt_hosps_monthly_diffs
+--january is initial value, not diff
+ALTER SESSION SET current_schema = c##jdoe;
+CREATE VIEW rpt_hosps_monthly_diffs AS
+WITH monthly_counts AS (
+   SELECT
+      dd.year  AS year,
+      dd.month AS month,
+      COUNT(1) month_count
+   FROM 
+      dm_hospnfz.f_hospitalizations f
+      JOIN dm_hospnfz.dim_date dd ON f.dim_date_id = dd.id_date
+   WHERE
+      dd.year = 2022
+   GROUP BY
+      dd.year,
+      dd.month
+)
+SELECT
+   year || '/' || lpad(month, 2, '0') AS period,
+   month_count,
+   nvl(month_count - LAG(month_count, 1) OVER(partition by year ORDER BY year, month),month_count) AS diff_with_prev_month
+FROM
+   monthly_counts mc
 ORDER BY
-    HOSP_PERIOD
-;
+   year,
+   month ASC
+   ;
+
+SELECT
+   dd.month,
+   COUNT(1) month_count
+FROM
+        f_hospitalizations f
+   JOIN dim_date dd ON f.dim_date_id = dd.id_date
+WHERE
+   dd.year = 2022
+GROUP BY
+   dd.month
+
 EXIT;
